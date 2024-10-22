6c007815d9abcb8ecdb8af19b7c3eb4c79d182b3
elsa853 <elsa.bessonnet@epitech.digital>
2024-10-22 15:21:21 +0200

    push favicon

--- material-tailwind-project/src/components/Footer.jsx
+++ material-tailwind-project/src/components/Footer.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 import { Typography } from "@material-tailwind/react"
 
-import Logo from "../assets/Moodly-long.svg"
+import Logo from "../assets/logo_moodly.svg"
 
 
 function FooterWithLogo() {
     return (
-        <footer className="w-full bg-green-100 p-8" style={{ paddingTop: "100px" }}>
-            <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-green-100 text-center md:justify-between">
-            <img src={Logo} alt="Moodly" className="h-8" />
+        <footer className="w-full bg-green-50 p-8 mt-16">
+            <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-green-50 text-center md:justify-between">
+            <img src={Logo} alt="Moodly" className="h-14" />
                 <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
                     <li>
                         <Typography
--- material-tailwind-project/src/components/Navbar.jsx
+++ material-tailwind-project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Navbar, MobileNav, Typography, Button, IconButton } from "@material-tai
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
-import Logo from "../assets/Moodly.svg";
+import Logo from "../assets/logo_moodly.svg";
 import { Link } from "react-router-dom";
 
 
@@ -44,10 +44,7 @@ function MoodlyNavbar() {
         <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
             <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
                 <Link to="/" className="flex items-center gap-x-2">
-                    <img src={Logo} alt="Moodly" className="h-8 w-8" />
-                    <Typography color="blue-gray" className="text-2xl font-bold">
-                        Moodly
-                    </Typography>
+                    <img src={Logo} alt="Moodly" className="h-10 w-30" />
                 </Link>
                 <div className="flex items-center gap-x-1">
                     {user ? (
